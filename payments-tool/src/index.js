aa1dd05a296fcb4efca9c2695d077ea637b06836
Richard Birkby <rbirkby@gmail.com>
2017-07-05 13:57:42 +0100

    Initial commit

--- payments-tool/src/index.js
+++ payments-tool/src/index.js
@@ -3,5 +3,9 @@ import dependency from './lib/dependency';
 console.info('Payments tool dynamically loaded');
 
 export function render() {
+<<<<<<< HEAD
   return '<h2>Payments<h2>';
+=======
+  return '<h1>Payments<h1>';
+>>>>>>> d0a7c57... Initial commit
 }
